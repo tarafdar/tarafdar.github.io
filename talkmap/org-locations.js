8520ec3a004c02cdaddfffd184741c243216f54c
tarafdar <naif.tarafdar@mail.utoronto.ca>
2019-02-18 23:47:27 -0500

    update talkmap

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,1 +1,22 @@
-var addressPoints = [];
+var addressPoints = [
+  [
+    "San Jose, California", 
+    37.3361905, 
+    -121.8905833
+  ], 
+  [
+    "Monterrey, California", 
+    36.600256, 
+    -121.8946388
+  ], 
+  [
+    "Chicago, Illinois", 
+    41.8755616, 
+    -87.6244212
+  ], 
+  [
+    "Ghent, Belgium", 
+    51.0538286, 
+    3.7250121
+  ]
+];
